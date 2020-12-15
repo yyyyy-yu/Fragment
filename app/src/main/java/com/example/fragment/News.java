c0c756511264fb81e68dc0450438df90963e64ff
yyyyy-yu <2911464427@qq.com>
2020-12-15 21:09:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragment/News.java
@@ -0,0 +1,21 @@
+package com.example.fragment;
+
+/**
+ * Created by 29114 on 2020/12/15.
+ */
+public class News {
+    private String title;
+    private String content;
+    public String getTitle() {
+        return title;
+    }
+    public  void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
